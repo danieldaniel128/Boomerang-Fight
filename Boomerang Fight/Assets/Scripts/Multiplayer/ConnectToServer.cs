7818b298087e9d07f3a12263f1ad72eef95089f7
danieldaniel128 <107756664+danieldaniel128@users.noreply.github.com>
2024-01-13 14:40:00 +0200

    lobby login

--- Boomerang Fight/Assets/Scripts/Multiplayer/ConnectToServer.cs
+++ Boomerang Fight/Assets/Scripts/Multiplayer/ConnectToServer.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class ConnectToServer : MonoBehaviourPunCallbacks
 {
-    
+    [SerializeField] TMP_InputField _playerNameInput;
+    [SerializeField] TextMeshProUGUI _connectingState_TMP;
+    const string _lobbySceneName = "Lobby";
 
+    /// <summary>
+    /// tries to connect to server using setting and seting player nickname
+    /// </summary>
     public void ConnectPlayerToServer()
     {
+        PhotonNetwork.NickName = _playerNameInput.text;
+        _connectingState_TMP.text = "Connecting...";
         PhotonNetwork.ConnectUsingSettings();
     }
 
@@ -18,6 +27,6 @@ public class ConnectToServer : MonoBehaviourPunCallbacks
     /// </summary>
     public override void OnConnectedToMaster()
     {
-        SceneManager.LoadScene("Lobby");
+        SceneManager.LoadScene(_lobbySceneName);
     }
 }
