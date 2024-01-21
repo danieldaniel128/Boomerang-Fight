2980b55e916b02e677488f8233a440b5265e5d0f
danieldaniel128 <107756664+danieldaniel128@users.noreply.github.com>
2024-01-21 21:28:26 +0200

    spawn players

---
+++ Boomerang Fight/Assets/Scripts/Managers/Spawning/PlayerSpawner.cs
@@ -0,0 +1,35 @@
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviourPun
+{
+    [SerializeField] SpawnPoint[] _spawnPoints;
+    int index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //_spawnManager = new SpawnManager(_spawnPoints);
+        SpawnPlayers();
+    }
+
+    void SpawnPlayers()
+    {
+        if(PhotonNetwork.IsMasterClient)
+        {
+            foreach(Player pl in PhotonNetwork.PlayerList)
+            {
+                photonView.RPC(nameof(InstantiationPlayer),pl,index);
+                index++;
+            }
+        }
+    }
+
+    [PunRPC]
+    void InstantiationPlayer(int index)
+    {
+        PhotonNetwork.Instantiate("Player", _spawnPoints[index].SpawnPosition, Quaternion.identity, 0);
+    }
+}
---
+++ Boomerang Fight/Assets/SpawnPoint.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    public int ID;
+    public Vector3 SpawnPosition => transform.position;
+    public bool Occupied;
+}
