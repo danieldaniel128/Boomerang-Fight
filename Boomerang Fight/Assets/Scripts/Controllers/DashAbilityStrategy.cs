2ea31bce95fad344a368c1ae82be5e68e4b93d09
EladReuven <102731503+EladReuven@users.noreply.github.com>
2024-02-06 19:17:08 +0200

    Update DashAbilityStrategy.cs

--- Boomerang Fight/Assets/Scripts/Controllers/DashAbilityStrategy.cs
+++ Boomerang Fight/Assets/Scripts/Controllers/DashAbilityStrategy.cs
@@ -13,36 +13,39 @@ public class DashAbilityStrategy : MonoBehaviour
 
     bool _inDash;
     bool _canDash = true;
-    Vector3 dashDestination;
-    Vector3 dashStartPosition;
+    Vector3 _dashDestination;
+    Vector3 _dashStartPosition;
     CountdownTimer _dashCooldown;
     CountdownTimer _dashDuration;
 
 
-
-    private void Start()
+    private void OnEnable()
     {
         SetTimers();
 
         _canDash = true;
     }
+
     private void Update()
     {
-
-        if (_dashCooldown.IsRunning)
-        {
-            _dashCooldown.Tick(Time.deltaTime);
-        }
+        TickTimers();
 
         if (_dashDuration.IsRunning)
-        {
-            _dashDuration.Tick(Time.deltaTime);
             Dash();
-        }
-
     }
 
-    
+    private void OnDisable()
+    {
+        ClearTimers();
+    }
+
+    #region Timers
+
+    private void TickTimers()
+    {
+        _dashCooldown.Tick(Time.deltaTime);
+        _dashDuration.Tick(Time.deltaTime);
+    }
 
     private void SetTimers()
     {
@@ -52,26 +55,71 @@ public class DashAbilityStrategy : MonoBehaviour
         SetDurationTimer();
     }
 
+    private void ClearTimers()
+    {
+        ClearCooldownTimer();
+        ClearDurationTimer();
+    }
+
+    #region DurationTimer
     private void SetDurationTimer()
     {
         _dashDuration = new(_duration);
-        _dashDuration.OnTimerStart += () => _inDash = true;
-        _dashDuration.OnTimerStop += () => _inDash = false;
+        _dashDuration.OnTimerStart += ActivateDash;
+        _dashDuration.OnTimerStop += DeactivateDash;
     }
 
+    private void ActivateDash()
+    {
+        _inDash = true;
+    }
+
+    private void DeactivateDash()
+    {
+        _inDash = false;
+    }
+
+    private void ClearDurationTimer()
+    {
+        _dashDuration.OnTimerStart -= ActivateDash;
+        _dashDuration.OnTimerStop -= DeactivateDash;
+    }
+    #endregion DurationTimer
+
+    #region CooldownTimer
     private void SetCooldownTimer()
     {
         _dashCooldown = new(_cooldown);
-        _dashCooldown.OnTimerStart += () => _canDash = false;
-        _dashCooldown.OnTimerStop += () => _canDash = true;
+        _dashCooldown.OnTimerStart += EnableDash;
+        _dashCooldown.OnTimerStop += DisableDash;
     }
 
+    private void EnableDash()
+    {
+        _canDash = true;
+    }
+
+    private void DisableDash()
+    {
+        _canDash = false;
+    }
+
+    private void ClearCooldownTimer()
+    {
+        _dashCooldown.OnTimerStart -= EnableDash;
+        _dashCooldown.OnTimerStop -= DisableDash;
+    }
+
+    #endregion CooldownTimer
+
     [ContextMenu("Reset Timers")]
-    public void ResetTimers()
+    void ResetTimers()
     {
         _dashCooldown.Reset(_cooldown);
         _dashDuration.Reset(_duration);
     }
+
+    #endregion Timers
 
 
     [ContextMenu("TryDash")]
@@ -81,25 +129,20 @@ public class DashAbilityStrategy : MonoBehaviour
         if (_canDash)
         {
             //set dash destination
-            dashStartPosition = transform.position;
-            dashDestination = transform.position + transform.forward * _range;
-
-            print("sPos: " + dashStartPosition);
-            print("ePos: " + dashDestination);
-
-
+            _dashStartPosition = transform.position;
+            _dashDestination = _dashStartPosition + transform.forward * _range;
             //start timers
             _dashCooldown.Start();
             _dashDuration.Start();
         }
     }
 
-    void Dash()
+    private void Dash()
     {
         //lerp transform to facing direction + range
         float curveValue = _speedCurve.Evaluate(1 - _dashDuration.Progress);
-        Vector3 newPosition = Vector3.Lerp(dashStartPosition, dashDestination, curveValue);
-        
+        Vector3 newPosition = Vector3.Lerp(_dashStartPosition, _dashDestination, curveValue);
+
         transform.position = newPosition;
 
     }
