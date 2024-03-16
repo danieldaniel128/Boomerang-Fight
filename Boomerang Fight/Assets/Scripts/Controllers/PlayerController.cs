f3e2c2d96adf005222aa23b4d063935afe9cc6dc
EladReuven <102731503+EladReuven@users.noreply.github.com>
2024-03-16 12:54:47 +0200

    wall collision + angle check through transform movement

--- Boomerang Fight/Assets/Scripts/Controllers/PlayerController.cs
+++ Boomerang Fight/Assets/Scripts/Controllers/PlayerController.cs
@@ -5,11 +5,13 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviourPun
 {
 
     //[SerializeField] UserInputPhone _input;
+    [Header("Objects")]
     [SerializeField] GameObject _playerBody;
     [SerializeField] GameObject _boomerangVisual;
     [Header("Components")]
@@ -20,18 +22,30 @@ public class PlayerController : MonoBehaviourPun
     [SerializeField] MeleeAbility _meleeAbility;
     [SerializeField] PlayerAnimationController _playerAnimationController;
     [SerializeField] Boomerang _boomerang;
+    [SerializeField] Collider _bodyCollider;
     [Header("JoySticks Set-UP")]
     [SerializeField] GameObject _joystickCanvas;
     [SerializeField] Joystick _moveJoystick;
     [SerializeField] Joystick _AttackJoystick;
-    [Header("Player Stats")]
+    [Header("Player Movement Stats")]
     [SerializeField] float _moveSpeed;
+    [SerializeField] float _timeToAccelerate = 0.2f;
+    [SerializeField] float _timeToDecelerate = 0.2f;
+    [SerializeField] float _maxAngle = 80f;
+    [SerializeField] float _distanceToWallCheck = 2f;
     [Header("Actions")]
     public UnityEvent OnRecall;
 
     private Action OnMasterPlayerControllerUpdate;
     private Action OnLocalPlayerControllerUpdate;
     int _mySpawnIndex;
+    float _currentSpeed = 0f;
+    Vector3 _moveDirection = Vector3.zero;
+
+    float Acceleration => _moveSpeed / _timeToAccelerate;
+    float Deceleration => _moveSpeed / _timeToDecelerate;
+
+
     [PunRPC]
     void SetMyPlayerIndex(int index)
     {
@@ -105,19 +119,59 @@ public class PlayerController : MonoBehaviourPun
     }
     private void HandleMovement()
     {
-        Vector3 moveDirection = new Vector3(_moveJoystick.Horizontal, 0, _moveJoystick.Vertical).normalized;
-        transform.position += moveDirection * _moveSpeed * Time.deltaTime;
-        if (moveDirection.magnitude > 0)
+        //get movement from joystick
+        Vector3 inputDirection = new Vector3(_moveJoystick.Horizontal, 0, _moveJoystick.Vertical).normalized;
+        //check magnitude size
+        if (inputDirection.magnitude > 0)
         {
+            //movement
+            _currentSpeed = Mathf.MoveTowards(_currentSpeed, _moveSpeed, Acceleration * Time.deltaTime);
+            _moveDirection = inputDirection * _currentSpeed;
+
+            //animations
             _playerAnimationController.StartWalk();
-            _playerBody.transform.forward = moveDirection;
-            _dashAbility.UpdateDashDirection(moveDirection);
+            _playerBody.transform.forward = inputDirection;
+
+            //ability updates
+            _dashAbility.UpdateDashDirection(inputDirection);
         }
         else
         {
+            //movement
+            _currentSpeed = Mathf.MoveTowards(_currentSpeed, 0f, Deceleration * Time.deltaTime);
+            _moveDirection = _moveDirection.normalized * _currentSpeed;
+
+            //animations
             _playerAnimationController.StopWalk();
         }
+
+        //cast ray forward to check if wall/can move in that direction
+        //check if can move forward
+        Vector3 newPosition = AdjustMovementIfWall(_moveDirection * Time.deltaTime);
+        transform.position += newPosition;
     }
+
+    private Vector3 AdjustMovementIfWall(Vector3 MovementVector)
+    {
+        RaycastHit hit;
+        if(Physics.SphereCast(transform.position, _distanceToWallCheck / 2, MovementVector, out hit, _distanceToWallCheck))
+        {
+            print(Vector3.Angle(hit.normal, MovementVector.normalized)/2);
+            if (Vector3.Angle(hit.normal, MovementVector.normalized)/2 < _maxAngle)
+            {
+                // Adjust movement along the wall
+                MovementVector = Vector3.ProjectOnPlane(MovementVector, hit.normal);
+                Debug.DrawRay(hit.point, MovementVector.normalized, Color.red);
+            }
+            else
+            {
+                //stop if wall angle is too steep
+                MovementVector = Vector3.zero;
+            }
+        }
+        return MovementVector;
+    }
+
     private void LocalPlayerControlUpdate()
     {
         if (photonView.IsMine)
@@ -201,5 +255,13 @@ public class PlayerController : MonoBehaviourPun
     void FaceThrowDirection()
     {
         _playerBody.transform.forward = new Vector3(_AttackJoystick.Horizontal, 0, _AttackJoystick.Vertical).normalized;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position + _moveDirection * Time.deltaTime, _distanceToWallCheck);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position + _moveDirection.normalized * _distanceToWallCheck, _distanceToWallCheck / 2);
     }
 }
