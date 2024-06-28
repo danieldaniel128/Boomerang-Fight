145506a5ab302f5d70a4ceecbcd8f6cb77863e0d
EladReuven <102731503+EladReuven@users.noreply.github.com>
2024-06-28 10:10:20 +0300

    fixed shimmer applying to everyone

--- Boomerang Fight/Assets/Scripts/Controllers/Materials/InvincibilityShimmerScript.cs
+++ Boomerang Fight/Assets/Scripts/Controllers/Materials/InvincibilityShimmerScript.cs
@@ -5,8 +5,16 @@ using UnityEngine.Rendering;
 
 public class InvincibilityShimmerScript : MonoBehaviour
 {
+
     const string EFFECTBOOL = "_EFFECTBOOL";
+    [SerializeField] SkinnedMeshRenderer skinnedMeshRenderer;
     [SerializeField] Material shimmerMaterial;
+    private void Start()
+    {
+        //making a new material so that activating the material properties will only paply on the new clone material and not everyone
+        shimmerMaterial = new Material(shimmerMaterial);
+        skinnedMeshRenderer.SetMaterials(new List<Material> { skinnedMeshRenderer.materials[0], shimmerMaterial } );
+    }
     public void ActivateShimmer()
     {
         print("activate shimmer");
